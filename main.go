582d3961b01c82dbf11d84e437e3c5c498492089
Daniel Barnes <dabarnes2b@gmail.com>
2022-01-17 10:06:26 -0800

    basic

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"os"
 
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 	compute "google.golang.org/api/compute/v1"
-	"google.golang.org/api/option"
 )
 
 func main() {
-	ctx := context.Background()
 	credData := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS_DATA")
 	credPath := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
 	if len(credData) == 0 && len(credPath) == 0 {
@@ -24,10 +23,12 @@ func main() {
 		}
 		credData = string(jsondata)
 	}
-	client, err := compute.NewClient(ctx, option.WithCredentialsJSON([]byte(credData)))
+	credentials, err := google.CredentialsFromJSON(oauth2.NoContext, []byte(credData), compute.ComputeScope)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	client.InstanceList()
-	fmt.Println("1")
+	project := credentials.ProjectID
+	service, err := compute.New(oauth2.NewClient(oauth2.NoContext, credentials.TokenSource))
+	instances, err := service.Instances.List(project, "all").Do()
+	fmt.Println(len(instances.Items))
 }
