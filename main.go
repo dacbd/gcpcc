a3bc7a0de230d7b411b9e1c88ad6df5702a1c4f5
Daniel Barnes <dabarnes2b@gmail.com>
2022-04-28 02:17:00 +0900

    GitHub Actionify (#1)
    * init
    
    * set-output
    
    * dummy input
    
    * debug
    
    * debug
    
    * bad if
    
    * fix output
    
    * correct print
    
    * rm debug code
    
    * tweak
    
    * actions tweaks

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -12,28 +16,52 @@ import (
 
 func main() {
 	credData := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS_DATA")
-	credPath := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
-	if len(credData) == 0 && len(credPath) == 0 {
-		log.Fatalln("no GCP credentials provided")
+	var credentials *google.Credentials
+	var err error
+	// get credentials
+	if len(credData) != 0 {
+		credentials, err = google.CredentialsFromJSON(context.Background(), []byte(credData), compute.ComputeReadonlyScope)
+	} else {
+		credentials, err = google.FindDefaultCredentials(context.Background(), compute.ComputeReadonlyScope)
 	}
-	if len(credData) == 0 {
-		jsondata, err := os.ReadFile(credPath)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		credData = string(jsondata)
-	}
-	credentials, err := google.CredentialsFromJSON(oauth2.NoContext, []byte(credData), compute.ComputeScope)
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	// support OIDC
+	if credentials.ProjectID == "" {
+		// 	Coerce Credentials to handle GCP OIDC auth
+		//	Common ProjectID ENVs:
+		//		https://github.com/google-github-actions/auth/blob/b05f71482f54380997bcc43a29ef5007de7789b1/src/main.ts#L187-L191
+		//		https://github.com/hashicorp/terraform-provider-google/blob/d6734812e2c6a679334dcb46932f4b92729fa98c/google/provider.go#L64-L73
+		coercedProjectID := multiEnvLoadFirst([]string{
+			"CLOUDSDK_CORE_PROJECT",
+			"CLOUDSDK_PROJECT",
+			"GCLOUD_PROJECT",
+			"GCP_PROJECT",
+			"GOOGLE_CLOUD_PROJECT",
+			"GOOGLE_PROJECT",
+		})
+		if coercedProjectID == "" {
+			// last effort to load
+			fromCredentialsID, err := coerceOIDCCredentials(credentials.JSON)
+			if err != nil {
+				log.Fatalln(fmt.Errorf("couldn't extract the project identifier from the given credentials!: [%w]", err))
+			}
+			coercedProjectID = fromCredentialsID
+		}
+		credentials.ProjectID = coercedProjectID
+	}
+
+	// create serivce
 	project := credentials.ProjectID
-	service, err := compute.New(oauth2.NewClient(oauth2.NoContext, credentials.TokenSource))
+	service, err := compute.New(oauth2.NewClient(context.Background(), credentials.TokenSource))
 	if err != nil {
 		log.Fatalln(err)
 	}
 	instanceCount := 0
 
+	// count instances
 	zones, err := service.Zones.List(project).Do()
 	if err != nil {
 		log.Fatalln(err)
@@ -45,21 +73,32 @@ func main() {
 		}
 		instanceCount += len(instances.Items)
 	}
+	fmt.Printf("::set-output name=total::%v", instanceCount)
+}
 
-	/*
-		regions, err := service.Regions.List(project).Do()
-		if err != nil {
-			log.Fatalln(err)
+// https://github.com/iterative/terraform-provider-iterative/blob/b9cd04a981df2b1426a67c58d506bdf9669eca5e/iterative/gcp/provider.go#L355-L370
+func coerceOIDCCredentials(credentialsJSON []byte) (string, error) {
+	var credentials map[string]interface{}
+	if err := json.Unmarshal(credentialsJSON, &credentials); err != nil {
+		return "", err
+	}
+
+	if url, ok := credentials["service_account_impersonation_url"].(string); ok {
+		re := regexp.MustCompile("^https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/.+?@(?P<project>.+).iam.gserviceaccount.com:generateAccessToken$")
+		if match := re.FindStringSubmatch(url); match != nil {
+			return match[1], nil
 		}
-		for _, item := range regions.Items {
-			for _, zone := range item.Zones {
-				instances, err := service.Instances.List(project, zone).Do()
-				if err != nil {
-					log.Fatalln(err)
-				}
-				instanceCount += len(instances.Items)
-			}
+		return "", errors.New("failed to get project identifier from service_account_impersonation_url")
+	}
+
+	return "", errors.New("unable to load service_account_impersonation_url")
+}
+
+func multiEnvLoadFirst(envs []string) string {
+	for _, val := range envs {
+		if env_value := os.Getenv(val); env_value != "" {
+			return env_value
 		}
-	*/
-	fmt.Println(instanceCount)
+	}
+	return ""
 }
