66ed64c908f5af30d0530f3be0a2395d681e3a7b
Daniel Barnes <dabarnes2b@gmail.com>
2022-04-27 08:11:28 -0700

    debug

--- main.go
+++ main.go
@@ -22,8 +22,10 @@ func main() {
 	fmt.Println(len(credData))
 	if len(credData) == 0 {
 		credentials, err = google.CredentialsFromJSON(context.Background(), []byte(credData), compute.ComputeReadonlyScope)
+		fmt.Println("cred from json")
 	} else {
 		credentials, err = google.FindDefaultCredentials(context.Background(), compute.ComputeReadonlyScope)
+		fmt.Println("google find")
 	}
 	if err != nil {
 		log.Fatalln(err)
